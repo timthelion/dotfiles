e404879d74ce1863b3f1f58ab6d97b3b160b9231
Timothy Hobbs <timothyhobbs@seznam.cz>
2019-02-11 21:59:57 +0100

    Add fish config

---
+++ config.fish
@@ -0,0 +1,6 @@
+set -gx PLAN9 $HOME/plan9
+set -gx venti 127.0.0.1
+set -gx PATH /home/timothy/bin /home/timothy/.subuser/bin /home/timothy/.cargo/bin $PATH /home/timothy/plan9/bin /home/timothy/.guix-profile/bin
+set -gx INFOPATH /home/timothy/.guix-profile/share/info $PATH
+set -gx RUST_SRC_PATH /home/timothy/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
+set -gx GUIX_LOCPATH $HOME/.guix-profile/lib/locale
