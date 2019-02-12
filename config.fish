c7759341d82a712a9639e539df4ef1141ea75be2
Timothy Hobbs <timothy@hobbs.cz>
2019-02-12 14:05:29 +0100

    Update fish config with prompt and window titling

--- config.fish
+++ config.fish
@@ -4,3 +4,21 @@ set -gx PATH /home/timothy/bin /home/timothy/.subuser/bin /home/timothy/.cargo/b
 set -gx INFOPATH /home/timothy/.guix-profile/share/info $PATH
 set -gx RUST_SRC_PATH /home/timothy/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
 set -gx GUIX_LOCPATH $HOME/.guix-profile/lib/locale
+
+function fish_prompt
+    set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
+    echo -n 'meaningless:'(prompt_pwd)'['"$git_branch"'] > '
+end
+
+function fish_title
+    if set -q argv[1]
+        echo $argv[1]
+    else
+        echo $PWD
+    end
+end
+
+set -g -x EDITOR nvim
+set -g -x GNUPGHOME /home/timothy/.gnupg/trezor
+
+alias vim nvim
