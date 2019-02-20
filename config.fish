00ef4ac8545a514d890f28f1e0c2da936383af14
Timothy Hobbs <timothy@hobbs.cz>
2019-02-21 00:55:55 +0100

    Update fish config

--- config.fish
+++ config.fish
@@ -4,6 +4,9 @@ set -gx PATH /home/timothy/bin /home/timothy/.subuser/bin /home/timothy/.cargo/b
 set -gx INFOPATH /home/timothy/.guix-profile/share/info $PATH
 set -gx RUST_SRC_PATH /home/timothy/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
 set -gx GUIX_LOCPATH $HOME/.guix-profile/lib/locale
+set -gx GTK_IM_MODULE ibus
+set -gx XMODIFIERS @im=ibus
+set -gx QT_IM_MODULE ibus
 
 function fish_prompt
     set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
